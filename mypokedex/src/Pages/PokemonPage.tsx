ae5f6b8fb1d8dcfef3e10b6f5ec0dc39ed885f8c
Alain Bonneau <alaindev97@gmail.com>
2024-11-22 10:35:47 +0100

    add style to single pokemon page

--- mypokedex/src/Pages/PokemonPage.tsx
+++ mypokedex/src/Pages/PokemonPage.tsx
@@ -1,19 +1,43 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar/Navbar";
+import Loader from "../components/Loader/Loader";
 import axiosInstance from "../lib/axios/axios";
+
+interface Pokemon {
+  name: {
+    fr: string;
+  };
+  sprites: {
+    regular: string;
+  };
+  category: string;
+  pokedex_id: number;
+  stats: {
+    hp: number;
+    atk: number;
+    def: number;
+    spe_atk: number;
+    spe_def: number;
+    vit: number;
+  };
+}
 
 function PokemonPage() {
   const { pokemonId } = useParams();
-  const [pokemon, setPokemon] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
   async function getOnePokemon() {
     try {
+      setIsLoading(true);
+      setError(null);
       const response = await axiosInstance.get(`/pokemon/${pokemonId}`);
       setPokemon(response.data);
     } catch (error) {
-      console.error(error);
+      console.error("Erreur lors de la récupération du Pokémon :", error);
+      setError("Impossible de charger les informations du Pokémon.");
     } finally {
       setIsLoading(false);
     }
@@ -27,12 +51,39 @@ function PokemonPage() {
   return (
     <div className="pokemon-page-container">
       <Navbar />
-      {isLoading ? (
-        <p>Chargement des informations du Pokémon...</p>
-      ) : pokemon ? (
-        <h1>Mon pokemon : {pokemon.name.fr}</h1>
-      ) : (
-        <p>Pokémon introuvable.</p>
+
+      <div className="title-container">
+        {isLoading && <Loader />}
+        {!isLoading && error && <p className="text-red-500">{error}</p>}
+        {!isLoading && pokemon && (
+          <h1 className="text-center pt-6 pb-6 font-bold text-5xl text-[#CC4C41]">
+            {pokemon.name?.fr || "Nom inconnu"}
+          </h1>
+        )}
+      </div>
+
+      {!isLoading && !error && pokemon && (
+        <div className="pokemon-container border-4 rounded-2xl w-4/5 m-auto">
+          <div className="pokemon-img flex justify-center items-center">
+            <img
+              className="w-96"
+              src={pokemon.sprites.regular}
+              alt={`Image de ${pokemon.name.fr}`}
+            />
+          </div>
+          <div className="stats-container flex flex-col justify-center items-center">
+            <p>{`Hp : ${pokemon.stats?.hp ?? "Non disponible"}`}</p>
+            <p>{`Attaque : ${pokemon.stats?.atk ?? "Non disponible"}`}</p>
+            <p>{`Défense : ${pokemon.stats?.def ?? "Non disponible"}`}</p>
+            <p>{`Attaque spécial : ${
+              pokemon.stats?.spe_atk ?? "Non disponible"
+            }`}</p>
+            <p>{`Défense spécial : ${
+              pokemon.stats?.spe_def ?? "Non disponible"
+            }`}</p>
+            <p>{`Vitesse : ${pokemon.stats?.vit ?? "Non disponible"}`}</p>
+          </div>
+        </div>
       )}
     </div>
   );
