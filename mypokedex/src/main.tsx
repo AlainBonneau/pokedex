bb902014a00eec189dd93dc3383e511990071434
Alain Bonneau <alaindev97@gmail.com>
2025-01-18 19:41:53 +0100

    correct build

--- mypokedex/src/main.tsx
+++ mypokedex/src/main.tsx
@@ -1,10 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.tsx";
-import PokemonPage from "./Pages/PokemonPage.tsx";
-import About from "./Pages/About.tsx";
-import Page404 from "./Pages/Page404.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
