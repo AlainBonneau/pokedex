382eae42f91969fb70964413fa4b00d75dc2f3e7
Alain Bonneau <alaindev97@gmail.com>
2025-01-24 18:00:17 +0100

    add about content

--- mypokedex/src/Pages/About.tsx
+++ mypokedex/src/Pages/About.tsx
@@ -1,9 +1,23 @@
-function About() {
+const About = () => {
   return (
-    <div className="about-container">
-      <h1>Page à propos</h1>
+    <div className="about-container max-w-3xl mx-auto h-[80vh] text-center p-6 flex justify-center flex-col items-center">
+      <h1 className="text-3xl font-bold mb-6 text-[#CC4C41]">
+        À propos de ce site
+      </h1>
+      <p className="text-lg mb-4">
+        Ce site a été créé dans le cadre de mon portfolio pour mettre en avant
+        mes compétences en développement web. Il utilise React pour construire
+        une interface interactive et réactive, permettant la recherche en temps
+        réel et une navigation fluide.
+      </p>
+      <p className="text-lg">
+        L'objectif de ce projet est de démontrer ma capacité à développer des
+        applications modernes, fonctionnelles et élégantes, tout en utilisant
+        des outils comme TypeScript, Tailwind CSS et des API REST pour intégrer
+        des données dynamiques.
+      </p>
     </div>
   );
-}
+};
 
 export default About;
